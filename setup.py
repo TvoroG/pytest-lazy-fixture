9d7965047737aa1158bae55ba2c7fde5af41c191
Marsel <marszaripov@gmail.com>
2016-11-19 22:45:01 +0300

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-lazy-fixture',
-    version='0.1.0',
+    version='0.2.0',
     author='Marsel Zaripov',
     author_email='marszaripov@gmail.com',
     maintainer='Marsel Zaripov',
