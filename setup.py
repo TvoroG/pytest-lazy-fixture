053166eab07574c83a200402831760a1eb93dd57
Marsel <marszaripov@gmail.com>
2017-02-04 23:29:18 +0300

    0.3.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-lazy-fixture',
-    version='0.2.0',
+    version='0.3.0',
     author='Marsel Zaripov',
     author_email='marszaripov@gmail.com',
     maintainer='Marsel Zaripov',
