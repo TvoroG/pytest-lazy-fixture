2079734740eae5e837764397539ee3fa84792a27
TvoroG <marszaripov@gmail.com>
2019-11-20 13:47:52 +0300

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-lazy-fixture',
-    version='0.6.1',
+    version='0.6.2',
     author='Marsel Zaripov',
     author_email='marszaripov@gmail.com',
     maintainer='Marsel Zaripov',
